4d988d49d9e050f5fb05ddf590e1f5a66896fb01
Omkar Date <omkardate29@gmail.com>
2024-12-22 23:30:40 +0530

    feat(ui): remove unnecessary container class from sign-in page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -109,7 +109,7 @@ export default function SignInPage() {
         />
       </div>
 
-      <div className="container px-4 flex flex-col gap-6 items-center justify-center bg-gray-100">
+      <div className="px-4 flex flex-col gap-6 items-center justify-center bg-gray-100">
         <Card className="max-w-xl w-full">
           <Tabs defaultValue="saas">
             <div className="p-6 pb-4 space-y-4 border-b-2">
